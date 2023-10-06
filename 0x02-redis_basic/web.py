cc48cf1554b6524a43369e76b2d19a6b697cf0e9
root <wambuilucie7@gmail.com>
2023-10-06 14:02:23 -0700

    file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,11 +6,9 @@ import requests
 from functools import wraps
 from typing import Callable
 
-
 redis_store = redis.Redis()
 '''The module-level Redis instance.
 '''
-
 
 def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
@@ -19,16 +17,14 @@ def data_cacher(method: Callable) -> Callable:
     def invoker(url) -> str:
         '''The wrapper function for caching the output.
         '''
-        redis_store.incr(f'count:{url}')
+        redis_store.incr(f'count:{url}')  # Increment the count
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        redis_store.setex(f'result:{url}', 10, result)  # Cache with expiration time
         return result
     return invoker
-
 
 @data_cacher
 def get_page(url: str) -> str:
@@ -36,3 +32,9 @@ def get_page(url: str) -> str:
     and tracking the request.
     '''
     return requests.get(url).text
+
+if __name__ == "__main__":
+    # Test the get_page function with caching
+    url = "http://google.com"
+    content = get_page(url)
+    print(content)

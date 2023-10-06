762d6df3a7134fb3f4c97cf72e0d649135091a7f
root <wambuilucie7@gmail.com>
2023-10-06 14:32:40 -0700

    file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
+"""
+web cache and tracker
+"""
 import requests
+import redis
 from functools import wraps
-from typing import Callable
 
-redis_store = redis.Redis()
-'''The module-lvl Redis instance
-'''
+store = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
-    def invoker(url: str) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        # Initialize the count to 0 if it doesn't exist
-        redis_store.incr(f'count:{url}', amount=0)
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        # Remove the line that resets the count
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-    return invoker
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
 
 
-@data_cacher
+@count_url_access
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request. Caching is implemented using Redis.
-    '''
-    return requests.get(url).text
-
-
-if __name__ == "__main__":
-    # Test the get_page function with caching
-    url = "http://google.com"
-    content = get_page(url)
-    print(content)
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
